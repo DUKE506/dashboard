4b26c0c3c8f377f408d7545e39071d322560aa68
DUKE506 <dukebs1128@gmail.com>
2025-05-27 23:18:43 +0900

    feat : 지하철정보 ui 완료(삭제, 다시조회 구현해야함)

--- src/components/common/content/station.tsx
+++ src/components/common/content/station.tsx
@@ -1,4 +1,6 @@
 import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { lineColor } from "@/lib/station";
 import { useDataState } from "@/store/data-store";
 import { StationData, Subway } from "@/types/arrival-info";
 import React, { useEffect, useState } from "react";
@@ -6,9 +8,6 @@ import React, { useEffect, useState } from "react";
 const Station = () => {
   const { stationData, addStationArrivalData } = useDataState();
   const [station, setStation] = useState<string>("");
-  // useEffect(() => {
-  //   getStationArrivalData(station);
-  // }, []);
 
   const handleAddStation = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key !== "Enter" || station === null) return;
@@ -17,7 +16,7 @@ const Station = () => {
   };
 
   return (
-    <div>
+    <div className="h-full flex flex-col">
       <div className="p-2">
         <Input
           value={station}
@@ -26,13 +25,18 @@ const Station = () => {
           onKeyDown={(e) => handleAddStation(e)}
         />
       </div>
-      <div>
-        {stationData ? (
+      <div
+        className="h-full flex flex-col gap-2 overflow-auto
+      "
+      >
+        {stationData.length > 0 ? (
           stationData.map((s, i) => (
             <StationItem key={i} data={s}></StationItem>
           ))
         ) : (
-          <span>역을 추가하세요.</span>
+          <div className="flex h-full items-center justify-center">
+            <span className="text-sm ">지하철역을 추가하세요.</span>
+          </div>
         )}
       </div>
     </div>
@@ -45,14 +49,14 @@ interface StationItemProps {
 
 const StationItem = ({ data }: StationItemProps) => {
   return (
-    <div>
+    <div className="flex flex-col gap-2 p-2">
       <div>
-        <span className="text-sm">{data.name}</span>
+        <span className="text-sm font-bold">{data.name}</span>
       </div>
-      <div className="flex flex-col gap-2">
-        {data.subway.map((s, i) => (
-          <SubwayLineItem key={i} data={s} />
-        ))}
+      <div className="grid gap-4 grid-cols-2 ">
+        {data
+          ? data.subway?.map((s, i) => <SubwayLineItem key={i} data={s} />)
+          : null}
       </div>
     </div>
   );
@@ -63,29 +67,49 @@ interface SubwayLineItemProps {
 }
 
 const SubwayLineItem = ({ data }: SubwayLineItemProps) => {
+  const [backColor, setBackColor] = useState<string>("#000000");
+  useEffect(() => {
+    const getColor = async () => {
+      const color = await lineColor(data.name); // await 추가
+      setBackColor(color);
+    };
+    getColor();
+  }, [data.name]);
   return (
-    <div>
-      <div>
-        <span className="text-xs font-bold">{data.name}</span>
+    <div className="flex flex-col gap-1 h-full">
+      <div className="flex items-center">
+        <span
+          className={`text-xs text-white rounded-3xl text-center px-2 `}
+          style={{ backgroundColor: backColor }}
+        >
+          {data.name}
+        </span>
       </div>
-      <div className="flex gap-8">
+      <div className="flex gap-2 f-ull">
         {/* 상행 */}
-        <div>
-          {data.upLine.map((u, i) => (
-            <div key={i}>
-              <span className="text-xs">{u.bstatnNm}</span>
-              <span className="text-xs">{u.barvlDt}</span>
-            </div>
-          ))}
+        <div className="flex-1 flex flex-col gap-1">
+          {data.upLine.map((u, i) => {
+            const time = Math.floor(parseInt(u.barvlDt) / 60);
+            return (
+              <div key={i} className="w-full flex justify-between">
+                <span className="text-xs">{u.bstatnNm}</span>
+                <span className="text-xs">{time}분</span>
+              </div>
+            );
+          })}
         </div>
+        <div className="h-full w-[1px] bg-gray-300" />
         {/* 하행 */}
-        <div>
-          {data.downLine.map((d, i) => (
-            <div key={i}>
-              <span className="text-xs">{d.bstatnNm}</span>
-              <span className="text-xs">{d.barvlDt}</span>
-            </div>
-          ))}
+        <div className="flex-1 flex flex-col gap-1">
+          {data.downLine.map((d, i) => {
+            const time = Math.floor(parseInt(d.barvlDt) / 60);
+            return (
+              <div key={i} className="w-full flex justify-between">
+                <span className="text-xs">{d.bstatnNm}</span>
+                <span className="text-xs">{time}분</span>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/lib/station.ts
+++ src/lib/station.ts
@@ -43,3 +43,49 @@ export const subwayIdTransfer = (stationId: string) => {
       return "Unknown";
   }
 };
+
+export const lineColor = async (line: string) => {
+  switch (line) {
+    case "1호선":
+      return "#0052A4";
+    case "2호선":
+      return "#00A84D";
+    case "3호선":
+      return "#EF7C1C";
+    case "4호선":
+      return "#00A4E3";
+    case "5호선":
+      return "#996cac";
+    case "6호선":
+      return "#CD7F00";
+    case "7호선":
+      return "#747F00";
+    case "8호선":
+      return "#E6186C";
+    case "9호선":
+      return "#BDB092";
+    case "GTX-A":
+      return "#0032A0";
+    case "경의중앙선":
+      return "#77C4A3";
+    case "공항철도":
+      return "#0090D2";
+    case "경춘선":
+      return "#178C72";
+    case "수인분당선":
+      return "#FABE00";
+    case "신분당선":
+      return "#D31145";
+    case "경강선":
+      return "#0054A6";
+    case "우이신설선":
+      return "#B0CE18";
+    case "서해선":
+      return "#8FC31F";
+    case "신림선":
+      return "#789CA";
+
+    default:
+      return "#000000";
+  }
+};
